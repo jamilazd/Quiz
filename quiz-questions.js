fe9a692091d921e02bb548ab229e6fa4b5ad7ba8
Jam.zD <jzdavis1212@gmail.com>
2020-09-23 12:45:20 +0100

    first draft of quiz

---
+++ quiz-questions.js
@@ -0,0 +1,58 @@
+
+const myQuestions = [
+	{
+		question: "Gruyere is a cheese from which country?",
+		answers: {
+			a: 'Switzerland',
+			b: 'Austria',
+			c: 'France'
+		},
+		correctAnswer: 'c'
+    },
+    
+	{
+		question: "Which of these is NOT a type of mushroom?",
+		answers: {
+			a: 'King Oyster',
+			b: 'Chicken of the woods',
+			c: `Lion's mane`
+		},
+		correctAnswer: 'b'
+    },
+    
+    {
+        question: "Bobotie is the national dish of which country?",
+		answers: {
+			a: 'South Africa',
+			b: 'Ethiopia',
+			c: 'Kenya'
+		},
+		correctAnswer: 'a'
+
+    }, 
+
+    {
+        question: "Which of these chocolate bars does NOT contain nuts?",
+		answers: {
+			a: 'Picnic',
+			b: 'Double Decker',
+			c: 'Toblerone'
+		},
+		correctAnswer: 'b'
+    },
+
+    {
+        question: "Which flower does the spice Saffron come from?",
+		answers: {
+			a: 'Poppy',
+			b: 'Orchid',
+			c: 'Crocus'
+		},
+		correctAnswer: 'c'
+    }, 
+
+];
+
+
+
+
