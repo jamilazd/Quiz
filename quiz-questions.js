302103d03c586499179e9f80d5e819cc1de0dcbd
Jam.zD <jzdavis1212@gmail.com>
2020-09-30 12:42:37 +0100

    corrected syntax of array

--- quiz-questions.js
+++ quiz-questions.js
@@ -1,58 +1,46 @@
 //will import questions to index.js later 
+
 const myQuestions = [
+
 	{
 		question: "Gruyere is a cheese from which country?",
-		answers: {
-			a: 'Switzerland',
-			b: 'Austria',
-			c: 'France'
-		},
-		correctAnswer: 'c'
+		answerA: 'Switzerland',
+		answerB: 'Austria',
+		answerC: 'France',
+		correctAnswer: 'c', 
     },
     
 	{
 		question: "Which of these is NOT a type of mushroom?",
-		answers: {
-			a: 'King Oyster',
-			b: 'Chicken of the woods',
-			c: `Lion's mane`
-		},
-		correctAnswer: 'b'
+        answerA: 'King Oyster',
+        answerB: 'Chicken of the woods',
+        answerC: `Lion's mane`, 
+		correctAnswer: 'b', 
     },
     
     {
         question: "Bobotie is the national dish of which country?",
-		answers: {
-			a: 'South Africa',
-			b: 'Ethiopia',
-			c: 'Kenya'
-		},
-		correctAnswer: 'a'
+        answerA: 'South Africa',
+        answerB: 'Ethiopia',
+        answerC: 'Kenya', 
+		correctAnswer: 'a', 
 
     }, 
 
     {
         question: "Which of these chocolate bars does NOT contain nuts?",
-		answers: {
-			a: 'Picnic',
-			b: 'Double Decker',
-			c: 'Toblerone'
-		},
-		correctAnswer: 'b'
+        answerA: 'Picnic',
+        answerB: 'Double Decker',
+        answerC: 'Toblerone', 
+		correctAnswer: 'b', 
     },
 
     {
         question: "Which flower does the spice Saffron come from?",
-		answers: {
-			a: 'Poppy',
-			b: 'Orchid',
-			c: 'Crocus'
-		},
-		correctAnswer: 'c'
+        answerA: 'Poppy',
+        answerB: 'Orchid',
+        answerC: 'Crocus',
+        correctAnswer: 'c', 
     }, 
 
 ];
-
-
-
-
